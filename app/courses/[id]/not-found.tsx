a50a4b9cb13997844c3284e81d35a77c0c53a4e7
Anna-Bala <annabala.contact@gmail.com>
2024-08-24 13:29:31 +0200

    Add not found page

---
+++ app/courses/[id]/not-found.tsx
@@ -0,0 +1,22 @@
+import LinkButton, { linkButtonVariants } from "@/components/LinkButton";
+import Typography, { typographyColors, typographyVariants } from "@/components/Typography";
+
+export default function CourseDetailsNotFound({ params }: { params: { id: string } }) {
+  return (
+    <main className="flex min-h-screen flex-col items-center px-4 lg:px-5 2xl:px-[30px]">
+      <div className="flex flex-col items-center gap-5 w-full lg:gap-10 2xl:gap-[50px]">
+        <div>
+          <Typography color={typographyColors.orange50} htmlTag="span" variant={typographyVariants.h2}>
+            Sorry
+          </Typography>
+          <Typography color={typographyColors.grey10} htmlTag="span" variant={typographyVariants.h2}>
+            ,&nbsp;we couldn't find the course
+          </Typography>
+        </div>
+        <LinkButton className="w-max" href="/" variant={linkButtonVariants.primary}>
+          Go back to home page
+        </LinkButton>
+      </div>
+    </main>
+  );
+}
