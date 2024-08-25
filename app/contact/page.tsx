05ba7dd0234af06cbd1cae06e04f5f5c51faa188
Anna-Bala <annabala.contact@gmail.com>
2024-08-25 17:48:08 +0200

    Add desktop layout for contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,3 @@
-import type { ReactNode } from "react";
-
 import { LocationIcon, MailIcon, PhoneIcon } from "@/icons";
 import { socialLinks } from "@/app/constants";
 import PageHeader from "@/components/PageHeader";
@@ -21,25 +19,26 @@ export default function Contact() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center px-4 lg:px-5 2xl:px-[30px]">
+    <main className="flex flex-col items-center px-4 lg:px-5 2xl:px-[30px]">
       <PageHeader
         title="Contact Us"
+        titleClassName="lg:mt-[55px]"
         info="Welcome to SkillBridge's Contact page! Whether you have questions, need support, or want to discuss partnership opportunities, we're here to help. Reach out using the contact options below, and we'll assist you promptly. Your journey with SkillBridge matters to us, and we're dedicated to supporting your professional development. Let's connect!"
       />
-      <div className="flex flex-col gap-[50px] w-full lg:px-[60px] 2xl:px-[142px] lg:gap-[80px] 2xl:gap-[100px] bg-absolute-white rounded-xl">
-        <div className="flex flex-col">
-          <form className="flex flex-col gap-5 p-[30px]">
-            <TextInput label="First Name" labelClassName="mb-3" name="firstName" placeholder="Enter First Name" />
-            <TextInput label="Last Name" labelClassName="mb-3" name="lastName" placeholder="Enter Last Name" />
-            <TextInput label="Email" labelClassName="mb-3" name="email" placeholder="Enter your Email" type="email" />
-            <TextInput label="Phone" labelClassName="mb-3" name="phone" placeholder="Enter Phone Number" type="tel" />
-            <TextInput label="Subject" labelClassName="mb-3" name="subject" placeholder="Enter your Subject" />
-            <TextInput label="Message" labelClassName="mb-3" name="message" placeholder="Enter your Message here..." rows={5} />
-            <button className="text-absolute-white text-[14px] font-medium rounded-md bg-orange-50 hover:bg-orange-70 py-[14px] px-5 mt-[10px]" type="submit">
+      <div className="flex flex-col gap-[50px] w-full lg:px-[60px] 2xl:px-[142px] lg:gap-[80px] 2xl:gap-[100px]">
+        <div className="flex flex-col bg-absolute-white rounded-xl lg:flex-row lg:justify-between">
+          <form className="flex flex-col gap-5 p-[30px] lg:p-[60px] lg:flex-row lg:flex-wrap lg:gap-6 lg:border-r lg:border-white-97 2xl:lg:p-[80px] 2xl:gap-[30px]">
+            <TextInput className="lg:w-5/12 lg:flex-grow" label="First Name" labelClassName="mb-3 2xl:mb-4" name="firstName" placeholder="Enter First Name" />
+            <TextInput className="lg:w-5/12 lg:flex-grow" label="Last Name" labelClassName="mb-3 2xl:mb-4" name="lastName" placeholder="Enter Last Name" />
+            <TextInput className="lg:w-5/12 lg:flex-grow" label="Email" labelClassName="mb-3 2xl:mb-4" name="email" placeholder="Enter your Email" type="email" />
+            <TextInput className="lg:w-5/12 lg:flex-grow" label="Phone" labelClassName="mb-3 2xl:mb-4" name="phone" placeholder="Enter Phone Number" type="tel" />
+            <TextInput className="lg:w-full" label="Subject" labelClassName="mb-3 2xl:mb-4" name="subject" placeholder="Enter your Subject" />
+            <TextInput className="lg:w-full lg:mt-4" label="Message" labelClassName="mb-3 2xl:mb-4" name="message" placeholder="Enter your Message here..." rows={5} />
+            <button className="text-absolute-white text-[14px] font-medium rounded-md bg-orange-50 hover:bg-orange-70 py-[14px] px-5 mt-[10px] lg:mx-auto 2xl:mt-5 2xl:text-[18px]" type="submit">
               Send Your Message
             </button>
           </form>
-          <div className="flex flex-col gap-5 p-[30px] border-t border-white-97">
+          <div className="flex flex-col gap-5 p-[30px] border-t border-white-97 lg:p-0 lg:border-t-0 lg:px-[60px] lg:justify-center lg:gap-6 2xl:px-[80px] 2xl:gap-[50px]">
             {renderContactItem({ href: "mailto:support@skillbridge.com", Icon: MailIcon, text: "support@skillbridge.com" })}
             {renderContactItem({ href: "tel:+910000000000", Icon: PhoneIcon, text: "+91 00000 00000" })}
             {renderContactItem({
--- components/form/TextInput.tsx
+++ components/form/TextInput.tsx
@@ -3,6 +3,7 @@ import type { HTMLInputTypeAttribute } from "react";
 import Typography, { typographyColors, typographyVariants } from "@/components/Typography";
 
 type Props = {
+  className?: string;
   label: string;
   labelClassName: string;
   name: string;
@@ -11,12 +12,12 @@ type Props = {
   type?: HTMLInputTypeAttribute;
 };
 
-export default function TextInput({ label, labelClassName, name, placeholder, rows, type = "text" }: Props) {
+export default function TextInput({ className, label, labelClassName, name, placeholder, rows, type = "text" }: Props) {
   const inputClassNames =
     "p-5 bg-white-99 border border-white-95 rounded-md placeholder-grey-40 text-sm text-grey-20 outline-none transition transition-border duration-300 ease-in-out focus:ring-0 focus:border-1 focus:border-orange-50 lg:text-base 2xl:py-6 2xl:text-lg";
 
   return (
-    <div className="flex flex-col">
+    <div className={`flex flex-col ${className}`}>
       <Typography color={typographyColors.grey15} className={`select-none	${labelClassName}`} htmlFor={name} variant={typographyVariants.label}>
         {label}
       </Typography>
