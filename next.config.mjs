2b6681e16eab2f5184cb7223b458df6806f4c3e3
Anna-Bala <annabala.contact@gmail.com>
2024-09-24 23:25:24 +0200

    Change images hosting service

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "imgbox.com",
+        hostname: "ik.imagekit.io",
         port: "",
       },
     ],
