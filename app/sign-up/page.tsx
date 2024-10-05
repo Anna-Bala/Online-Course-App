a01683d26adb35c519da3b353a562ed921a220f6
Anna-Bala <annabala.contact@gmail.com>
2024-10-05 20:39:15 +0200

    Protect login and sign-up routes when user is logged in

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,7 +1,14 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
 import LoginOrSignUpPanel from "@/components/LoginOrSignUpPanel";
 import Testimonials from "@/components/testimonials/Testimonials";
 
-export default function SignUp() {
+export default async function SignUp() {
+  const activeSession = await getServerSession();
+
+  if (activeSession) redirect("/");
+
   return (
     <main className="flex flex-col items-baseline min-h-screen px-4 gap-[50px] lg:flex-row-reverse lg:gap-[80px] lg:px-[80px] 2xl:px-[165px] 2xl:gap-[100px]">
       <LoginOrSignUpPanel isSignUp />
